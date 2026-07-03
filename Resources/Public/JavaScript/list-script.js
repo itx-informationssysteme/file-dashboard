a614760ef7f3cd9a12c9b84cd39d01ed2e22e2dd
Patrik Marosváry <patrik.marosvary@itx.de>
2026-07-03 15:39:05 +0200

    fix: Fixed download all button positioning, codestyle fixes

--- Resources/Public/JavaScript/list-script.js
+++ Resources/Public/JavaScript/list-script.js
@@ -68,10 +68,10 @@ document.addEventListener('DOMContentLoaded', function () {
 	}
 
 	const updateSelectedSize = () => {
-		let total = 0;
-	
-		const checkedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked);
-	
+		let total = 0
+
+		const checkedCheckboxes = Array.from(checkboxes).filter(checkbox => checkbox.checked)
+
 		checkedCheckboxes.forEach(checkbox => {
 			files.every(file => {
 				if (file.identifier == checkbox.value) {
@@ -86,17 +86,17 @@ document.addEventListener('DOMContentLoaded', function () {
 		if (selectedSizeValue) {
 			selectedSizeValue.innerHTML = formatBytes(total)
 		}
-	};
+	}
 
 	function formatBytes(bytes, decimals = 2) {
 		if (!+bytes) return '0 Bytes'
-	
+
 		const k = 1024
 		const dm = decimals < 0 ? 0 : decimals
 		const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-	
+
 		const i = Math.floor(Math.log(bytes) / Math.log(k))
-	
+
 		return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
 	}
 
